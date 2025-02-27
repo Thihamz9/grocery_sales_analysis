66781d341cbca5731d6ca77bca6ae47c49517ab6
ThihaOnTheBlock <thihaofficial123@gmail.com>
2025-02-27 06:18:43 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+<h1>Green Mall (2017-2018) Grocery Sales Analysis</h1>
+
+<h2>Project Background</h2>
+<p>Green Mall, established in 1980, rapidly expanded to become a leading multinational retail company with over 4,000 grocery supermarkets worldwide. In less than three decades, it has demonstrated significant growth by establishing a strong presence in several countries.
+
+The aim of this project is to leverage data analytics to understand sales trends and consumer behaviors during 2017 and 2018. By analyzing this data, Green Mall intends to enhance business decision-making and tailor its strategies based on the uncovered trends and patterns. </p>
+
+<h2>Dataset Overview</h2>
+<p>The dataset comprises sales records from 2017 and 2018, sourced from various Green Mall supermarket locations around the globe. Each record includes details such as product name, product category, sales quantity, sales amount, date of sale, and location of the supermarket. The dataset encompasses over 2 million entries, offering a robust base for comprehensive analysis.</p>
+
+<img src='db_schema.png' style="width:60%;margin-left:20%;">
+
+<h2>Executive Summary</h2>
+
+<h2>Analysis & Insights</h2>
+
+<h2>Recommendations</h2>
+
+<h2>Assumptions & Caveats</h2>
---
+++ db_setup_sql/initial.sql
@@ -0,0 +1,119 @@
+CREATE TABLE categories(
+	category_id INT,
+	category_name VARCHAR(100)
+);
+
+COPY categories
+FROM 'C:\\Users\\thiha\\OneDrive\\Desktop\\DataAnalyticsProjects\\GROCERY_SALES_ANALYSIS\\categories.csv'
+DELIMITER ','
+CSV HEADER;
+
+CREATE TABLE cities(
+	city_id INT NOT NULL,
+	city_name VARCHAR(255),
+	zipcode INT,
+	country_id INT,
+	PRIMARY KEY (city_id)
+);
+
+COPY cities
+FROM 'C:\\Users\thiha\OneDrive\\Desktop\\DataAnalyticsProjects\\GROCERY_SALES_ANALYSIS\\cities.csv'
+DELIMITER ','
+CSV HEADER;
+
+SELECT * FROM cities;
+
+CREATE TABLE countries(
+	country_id INT NOT NULL,
+	country_name VARCHAR(100),
+	country_code VARCHAR(10),
+	PRIMARY KEY (country_id)
+);
+
+COPY countries
+FROM 'C:\\Users\thiha\OneDrive\\Desktop\\DataAnalyticsProjects\\GROCERY_SALES_ANALYSIS\\countries.csv'
+DELIMITER ','
+CSV HEADER;
+
+SELECT * FROM countries;
+
+CREATE TABLE customers (
+	customer_id INT NOT NULL,
+	first_name VARCHAR(255),
+	middle_initial VARCHAR(255),
+	last_name VARCHAR(255),
+	city_id INT,
+	address VARCHAR(255),
+	PRIMARY KEY(customer_id)
+);
+
+COPY customers
+FROM 'C:\\Users\thiha\OneDrive\\Desktop\\DataAnalyticsProjects\\GROCERY_SALES_ANALYSIS\\customers.csv'
+DELIMITER ','
+CSV HEADER;
+
+SELECT * FROM customers;
+
+CREATE TABLE employees (
+	employee_id INT NOT NULL,
+	first_name VARCHAR(255),
+	middle_initial VARCHAR(255),
+	last_name VARCHAR(255),
+	birth_date DATE,
+	gender CHAR(1),
+	city_id INT,
+	hire_date TIMESTAMP,
+	PRIMARY KEY(employee_id)
+	
+);
+
+COPY employees
+FROM 'C:\\Users\thiha\OneDrive\\Desktop\\DataAnalyticsProjects\\GROCERY_SALES_ANALYSIS\\employees.csv'
+DELIMITER ','
+CSV HEADER;
+
+SELECT * FROM employees;
+
+
+
+CREATE TABLE products (
+	product_id INT NOT NULL,
+	product_name VARCHAR(255),
+	price NUMERIC(10,2),
+	category_id INT,
+	class_type VARCHAR(50),
+	modify_date TIMESTAMP,
+	resistant VARCHAR(50),
+	is_allergic BOOLEAN,
+	vitality_days FLOAT,
+	PRIMARY KEY(product_id)
+);
+
+COPY products
+FROM 'C:\\Users\thiha\OneDrive\\Desktop\\DataAnalyticsProjects\\GROCERY_SALES_ANALYSIS\\products.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ',', NULL 'Unknown');
+
+SELECT * FROM products;
+
+
+CREATE TABLE sales (
+    sales_id INT PRIMARY KEY,
+    sales_person_id INT,
+    customer_id INT,
+    product_id INT,
+    quantity INT,
+    discount NUMERIC(5,2), 
+    total_price NUMERIC(10,2), 
+    sales_date TIMESTAMP, 
+    transaction_number VARCHAR(255) 
+);
+
+COPY sales
+FROM 'C:\\Users\thiha\OneDrive\\Desktop\\DataAnalyticsProjects\\GROCERY_SALES_ANALYSIS\\sales.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ',');
+
+SELECT * FROM sales;
+
+
+
+
